cda0ca42ed33bce06af662881c88c84cef1c6de3
Mike Prokofyev <mikeprokofyev@gmail.com>
2015-12-12 10:12:55 +0500

    init

---
+++ Assets/Scripts/PlayerGraphics.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGraphics : MonoBehaviour {
+
+	SpriteRenderer playerRenderer;
+	Animator animator;
+
+	void Awake() {
+		playerRenderer = GetComponent<SpriteRenderer> ();
+		animator = GetComponent<Animator> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ButtonPressed() {
+		animator.SetTrigger("action");
+	}
+
+	public void FlipX (bool flipX) {
+		playerRenderer.flipX = flipX;
+	}
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+	SpriteRenderer renderer;
+	PlayerGraphics graphics;
+
+	void Awake () {
+		graphics = GetComponent<PlayerGraphics> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandleKeys();
+	}
+
+	void HandleKeys () {
+		float horizontalInput = Input.GetAxis("Horizontal");
+		if (horizontalInput != 0) {
+			graphics.FlipX(horizontalInput < 0);
+			graphics.ButtonPressed();
+
+		}
+	}
+
+
+}
